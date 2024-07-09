20cb8d260bf0b7ea0a202dd38b73227ce6377b03
javiersierrasoftware <javiersierrasoftware@gmail.com>
2024-07-09 08:29:11 -0500

    Ajuste de logo

--- screens/SplashScreen.js
+++ screens/SplashScreen.js
@@ -5,9 +5,9 @@ import imagenes from '../assets/img/imagenes';
 
 export default function SplashScreen() {
     return (
-        <View style={tw`flex-1 justify-center items-center bg-white`}>
-            <Image source={imagenes.logoPrisma} style={tw`w-32 h-32`} resizeMode='contain' />
-            <Text style={tw`text-4xl font-bold text-gray-900 mt-6`}>PrismaU</Text>
+        <View style={tw`flex-1 justify-center items-center bg-white p-6`}>
+            <Image source={imagenes.logoPrisma} style={tw`w-40 h-40`} resizeMode='contain' />
+            <Text style={tw`text-4xl font-bold text-gray-900 mt-8`}>PrismaU</Text>
         </View>
     );
 };
