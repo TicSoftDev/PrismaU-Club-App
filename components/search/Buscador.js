db32128a1d6522ae37e6dc98810cf8a2cc09f84c
javiersierrasoftware <javiersierrasoftware@gmail.com>
2024-08-17 10:30:10 -0500

    Se ajusto teclado numerico en busqueda por identificación

--- components/search/Buscador.js
+++ components/search/Buscador.js
@@ -8,8 +8,13 @@ export default function Buscador({ busqueda, handleChange, handleBusqueda }) {
         <View style={tw`flex flex-row items-center justify-center p-5 m-5`}>
             <View style={tw`flex flex-row items-center border rounded-xl p-2 border-gray-300`}>
                 <Icon name="id-card" size={20} color="#000" style={tw`ml-2 mr-4 text-gray-500`} />
-                <TextInput style={tw`text-lg flex-1`} placeholder="Busque por identificación."
-                    value={busqueda} onChangeText={(text) => handleChange(text)} />
+                <TextInput 
+                    style={tw`text-lg flex-1`} 
+                    placeholder="Busque por identificación."
+                    value={busqueda} 
+                    onChangeText={(text) => handleChange(text)} 
+                    keyboardType="numeric"  // Activar el teclado numérico
+                />
             </View>
             <TouchableOpacity onPress={() => handleBusqueda()} style={tw`ml-2 bg-green-500 p-3 rounded-xl justify-center items-center`}>
                 <Icon name="search" size={20} color="white" />
